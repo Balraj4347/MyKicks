1a415f8054dd91c40fc998997e8df748d051e28c
Balraj Gahlot <balrajgahlot20@gmail.com>
2022-06-13 19:03:45 +0530

    homepage setup

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,24 +1,22 @@
 import React from "react";
+import "../Styles/HomePage.css";
+import bgVideo from "../components/Media/CLIP_MyKicks_homepage.mp4";
 
 const HomePage = () => {
   return (
-    <div style={{ minHeight: "60vh" }}>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
-      <div style={{ height: "10vh" }}>123 Page</div>
+    <div className='homepage-container'>
+      <section className='homepage-section-1'>
+        <video autoPlay loop muted>
+          <source src={bgVideo} type='video/mp4' />
+        </video>
+        <div className='hp-section1-text'>
+          <h3>MyKicks</h3>
+          <p>Life isn't Perfect but your Sneaker can be</p>
+        </div>
+      </section>
+      <section className='aasdsada'>section 2</section>
+      <section className='aasdsada'>section 3</section>
+      <section className='aasdsada'>section 4</section>
     </div>
   );
 };
