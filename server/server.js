5f3bc9da10d63cde9e9684d95843802efc3e3873
Balraj Gahlot <balrajgahlot20@gmail.com>
2022-06-18 18:34:40 +0530

    server init

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const { path } = require("./app");
 
 // ----- config for dotenv --------
-dotenv.config({ path: "backend/config/config.env" });
+dotenv.config({ path: "server/config/config.env" });
 
 app.listen(process.env.PORT, () => {
   console.log(`Service Running on http://localhost:${process.env.PORT}`);
