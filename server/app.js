4e1bace2a26522ba935b6d0b8441e70fffd24543
Balraj Gahlot <balrajgahlot20@gmail.com>
2022-06-23 20:25:51 +0530

    middleware morgan error resolved

--- server/app.js
+++ server/app.js
@@ -1,16 +1,31 @@
-const bodyParser = require("body-parser");
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
 const app = express();
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "server/config/config.env" });
+}
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === "DEVELOPMENT") {
+  app.use(morgan("dev"));
+}
 
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
 const orderRoute = require("./routes/orderRoute");
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use("/api", productRoute);
 app.use("/api", userRoute);
 app.use("/api", orderRoute);
+
+app.get("/", (req, res) => {
+  res.send("Server is Running! 🚀");
+});
 
 module.exports = app;
