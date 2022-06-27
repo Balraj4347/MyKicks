cd4e0bbaea1eb4702cac31f1a49129ee3368155d
Balraj Gahlot <balrajgahlot20@gmail.com>
2022-06-27 18:17:12 +0530

    css changes

--- client/src/components/Products/Product.jsx
+++ client/src/components/Products/Product.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ brand, description, name, images, price }) => {
+  return (
+    <div>
+      <p>{brand}</p>
+      <p>{name}</p>
+      <p>{description}</p>
+      {/* <p>{images}</p> */}
+      <p>{price}</p>
+    </div>
+  );
 };
 
 export default Product;
--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -2,16 +2,33 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getProducts } from "../../redux-actions/productAction";
+import Product from "./Product";
 
 const Products = () => {
   const dispatch = useDispatch();
-  const productData = useSelector((state) => state.products);
+  const { loading, productsCount, products, error } = useSelector(
+    (state) => state.products
+  );
 
   useEffect(() => {
     dispatch(getProducts());
   }, [dispatch]);
-  console.log(productData);
-  return <div>Products</div>;
+  console.log(loading, productsCount, products, error);
+  return (
+    <div>
+      Products
+      {!loading &&
+        products.map((product) => {
+          console.log(product);
+          return (
+            <>
+              <hr />
+              <Product {...product} key={product._id} />
+            </>
+          );
+        })}
+    </div>
+  );
 };
 
 export default Products;
