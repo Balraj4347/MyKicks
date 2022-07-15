962a8945617eb925fb52c5d9f7f7fb154d9cb2c8
Balraj Gahlot <balrajgahlot20@gmail.com>
2022-07-15 22:51:17 +0530

    admin controllers in product

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -6,10 +6,23 @@ const {
   getAllProducts,
   getProductDetails,
   createProduct,
+  getAdminProducts,
+  updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 
 router.route("/products").get(getAllProducts);
 router.route("/product/:id").get(getProductDetails);
-router.route("/createproduct").post(createProduct);
 
+router
+  .route("/admin/products")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
+router
+  .route("/admin/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+
+router
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 module.exports = router;
