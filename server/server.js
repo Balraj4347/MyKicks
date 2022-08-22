778cd48f7242a2c824f1b45f7663acfbb9fda70f
Balraj Gahlot <balrajgahlot20@gmail.com>
2022-08-22 16:37:06 +0530

    env changes

--- server/server.js
+++ server/server.js
@@ -16,8 +16,8 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-const server = app.listen(process.env.PORT, (req, res) => {
+const PORT = process.env.PORT || 8000;
+const server = app.listen(PORT, (req, res) => {
   console.log(`Service Running on http://localhost:${process.env.PORT}`);
 });
 
